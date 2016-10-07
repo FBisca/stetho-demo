ec8ed40ea23577ee57d25598c7e7d28727d2bf5a
FBisca <felipe15yo@gmail.com>
2016-10-07 15:52:20 -0300

    Adding release

--- StethoDemo/app/src/release/java/com/bisca/stethodemo/di/module/DebugModule.java
+++ StethoDemo/app/src/release/java/com/bisca/stethodemo/di/module/DebugModule.java
@@ -1,6 +1,11 @@
 package com.bisca.stethodemo.di.module;
 
+import javax.inject.Singleton;
+
+import com.bisca.stethodemo.StethoInitializer;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class DebugModule {
--- StethoDemo/app/src/release/java/com/bisca/stethodemo/di/module/StethoInitializerNoOp.java
+++ StethoDemo/app/src/release/java/com/bisca/stethodemo/di/module/StethoInitializerNoOp.java
@@ -3,8 +3,6 @@ package com.bisca.stethodemo.di.module;
 import android.content.Context;
 
 import com.bisca.stethodemo.StethoInitializer;
-import com.facebook.stetho.Stetho;
-import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import okhttp3.OkHttpClient;
 
